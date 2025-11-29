a26fcfa10389e48c044006551db57253f745ce17
jesus david fierro rivera <161945299+JesusDavid1405@users.noreply.github.com>
2025-11-29 00:13:06 -0500

    fix(environment): Environment correction, deployed backend URL

--- GESCOMPAH/src/api/constant/config.ts
+++ GESCOMPAH/src/api/constant/config.ts
@@ -1,5 +1,5 @@
 // src/api/constant/config.ts
-export const API_BASE_URL = "http://192.168.1.26:5163/api"; 
+export const API_BASE_URL = "https://gescomph-api-dev.nicemushroom-3213f79c.eastus2.azurecontainerapps.io/api"; 
 
 export const DEFAULT_HEADERS = {
   "Content-Type": "application/json",
